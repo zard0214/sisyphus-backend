b789fb1e6977ce16e6b45312d6e17c03d325f31c
zard0214 <z15168385776@gmail.com>
2021-06-15 12:16:05 +0800

    sentinel

---
+++ sisyphus-gateway/src/main/java/com/sisyphus/gateway/config/GatewayConfig.java
@@ -0,0 +1,84 @@
+package com.sisyphus.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayParamFlowItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author zhecheng.zhao
+ * @date Created in 15/06/2021 09:51
+ */
+@Configuration
+public class GatewayConfig {
+
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GatewayConfig(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        return new SentinelFallbackHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+
+    @PostConstruct
+    public void doInit() {
+        initGatewayRules();
+        initDegradeRules();
+    }
+
+
+    private void initGatewayRules() {
+        Set<GatewayFlowRule> rules = new HashSet<>();
+         rules.add(new GatewayFlowRule("sisyphus-authorize")
+                .setCount(1)
+                .setIntervalSec(1)
+        );
+        GatewayRuleManager.loadRules(rules);
+    }
+
+    private void initDegradeRules() {
+        DegradeRule degradeRule = new DegradeRule("sisyphus-provider-api") // 资源名称
+                    .setGrade(RuleConstant.DEGRADE_GRADE_EXCEPTION_RATIO) // 异常比率模式(秒级)
+                    .setCount(0.5) // 异常比率阈值(50%)
+                    .setMinRequestAmount(10)
+                    .setTimeWindow(10); // 熔断降级时间(10s)
+        DegradeRuleManager.loadRules(Collections.singletonList(degradeRule));
+    }
+
+}
--- sisyphus-gateway/src/main/java/com/sisyphus/gateway/config/SentinelFallbackHandler.java
+++ sisyphus-gateway/src/main/java/com/sisyphus/gateway/config/SentinelFallbackHandler.java
@@ -1,27 +1,65 @@
 package com.sisyphus.gateway.config;
 
-import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @author zhecheng.zhao
  * @date Created in 22/05/2021 12:16
  */
 @Component
-public class SentinelFallbackHandler implements BlockRequestHandler {
+public class SentinelFallbackHandler extends SentinelGatewayBlockExceptionHandler {
+
+    private List<ViewResolver> viewResolvers;
+    private List<HttpMessageWriter<?>> messageWriters;
+
+    public SentinelFallbackHandler(List<ViewResolver> viewResolvers, ServerCodecConfigurer serverCodecConfigurer) {
+        super(viewResolvers,serverCodecConfigurer);
+        this.viewResolvers = viewResolvers;
+        this.messageWriters = serverCodecConfigurer.getWriters();
+    }
+
+    private Mono<Void> writeResponse(ServerResponse response, ServerWebExchange exchange) {
+        ServerHttpResponse serverHttpResponse = exchange.getResponse();
+        serverHttpResponse.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+        serverHttpResponse.setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
+        String data = "{\n" +
+                "\"code\": 429,\n" +
+                "\"message\": \"请求过于频繁, 请稍后再试\"\n" +
+                "}";
+        byte[] datas = data.getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = serverHttpResponse.bufferFactory().wrap(datas);
+        return serverHttpResponse.writeWith(Mono.just(buffer));
+    }
 
     @Override
-    public Mono<ServerResponse> handleRequest(ServerWebExchange exchange, Throwable t) {
-        return ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
-                .contentType(MediaType.valueOf("application/json;charset=UTF-8"))
-                .bodyValue("{\n" +
-                        "\"code\": 200,\n" +
-                        "\"message\": \"请求过于频繁, 请稍后再试\"\n" +
-                        "}");
+    public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
+
+        if(serverWebExchange.getResponse().isCommitted()){
+            return Mono.error(throwable);
+        }
+        if(!BlockException.isBlockException(throwable)){
+            return Mono.error(throwable);
+        }
+        return handleBlockedRequest(serverWebExchange, throwable).flatMap(response -> writeResponse(response, serverWebExchange));
     }
+
+    private Mono<ServerResponse> handleBlockedRequest(ServerWebExchange exchange, Throwable throwable) {
+        return GatewayCallbackManager.getBlockHandler().handleRequest(exchange, throwable);
+    }
+
 }
