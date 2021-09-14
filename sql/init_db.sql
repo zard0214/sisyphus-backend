268b665c1266a3b26f6dc7b8bd7b121512a0ffca
zard0214 <z15168385776@gmail.com>
2021-09-14 19:16:54 +0800

    pom change

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,2 +1,8 @@
--- DROP DATABASE IF EXISTS `sisyphus_uc`;
--- CREATE DATABASE sisyphus_uac;
+DROP DATABASE IF EXISTS `sisyphus_uac`;
+CREATE DATABASE sisyphus_uac;
+
+DROP DATABASE IF EXISTS `sisyphus_udc`;
+CREATE DATABASE sisyphus_udc;
+
+DROP DATABASE IF EXISTS `sisyphus_auth`;
+CREATE DATABASE sisyphus_auth;
